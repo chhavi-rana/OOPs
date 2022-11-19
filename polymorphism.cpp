dfbd75c048dfb335d3573f7cfe0e2778f220f712
= <chhavirana83@gmail.com>
2022-11-19 15:17:12 +0530

    Adding polymorphism code.

---
+++ polymorphism.cpp
@@ -0,0 +1,35 @@
+/* 
+Polymorphism in C++
+
+It simply means many forms.
+It refers to the ability of a variable, function, or object to take on multiple forms, 
+which are when the behavior of the same object or function is different in different contexts. 
+
+Two types of polymorphism:
+
+1. Compile time polymorphism: 
+    ** Polymorphism achieved at compile-time.
+    ** Also referred to as static or early binding.
+    ** It is implemented by:
+           a. Function Overloading
+           b. Operator Overloading
+    ** The function to be invoked is decided at the compile time only.
+
+
+2.  Run time polymorphism:
+     ** Polymorphism achieved at run-time.
+     ** Also referred to as late binding or dynamic-binding.
+     ** It is implemented by Function overriding using virtual funcitons
+     ** when functions are resolved at runtime rather than compile time. When a call to 
+        an overridden method is resolved dynamically at runtime rather than compile time.
+
+*/
+
+/* 
+
+Real life examples: 
+    Best exmample is my mother.
+    She is a lecturer to her students, daughter to my grandparents and a sister to my aunt and uncle. 
+    
+*/
+
